a115bcd5f9edbf54aa601adac5fc4e74f357238d
Jared Newell <jared.newell@gmail.com>
2015-06-06 16:21:02 -0700

    Initial Commit.  All tests pass 100%

---
+++ BitDoodle.py
@@ -0,0 +1,98 @@
+
+"""
+Bit Doodle!
+
+Description:
+    Bit Doodle is a class designed for working with variable length bit fields.  While the class can technically be
+    used for anything, it is catered towards IPv4 and IPv6 addresses.  The main methods are join and disjoin, which
+    will take a list of variable length fields and a list of values and join them together to a single value, or
+    disjoin which will take a single value and a list of bit lengths, and break the original value into its multiple
+    parts.
+
+join:
+    Description:
+        Given a list of bit lengths, and a list of values, join the values into a single number.  If the bit lengths
+        are exactly 32 or 128 bits, an ipaddress object is returned instead.
+
+    Inputs:
+        format_ : list of integers, each representing a bit length
+        values : list of integers, each representing a value
+            NOTE: format_ and values must be the same length, or an exception will be thrown.
+            NOTE: Each value must be between 0 and the max value of its corresponding bit-length or an exception
+                  will be thrown.
+
+    Outputs:      todo: allow user to specify output type
+        A single value that can be of the following types:
+        Int
+        Long
+        ipaddress object (of bit lengths were exactly 32 or 128)
+
+disjoin:
+    Description:
+        Given a single value and a list of bit lengths, disjoin the value into its bit length values.
+
+    Inputs:
+        format_ : list of integers, each representing a bit length
+        value : A single value to be broken down into multiple pieces defined by format_
+
+    Outputs:
+        An array of integers or longs
+"""
+
+import ipaddress
+
+
+class BitDoodle:
+    def __init__(self, format_):
+        self.format_ = format_
+        self.total_bit_length = sum(self.format_)
+
+    def join(self, values):
+        # validate lists
+        self.validate_list_lengths(values)
+        real_values = []
+    
+        for index, bit_length in enumerate(self.format_):
+            # validate values
+            self.validate_value(values[index], bit_length)
+    
+            shift_distance = self.get_bit_shift_distance(self.format_, index)
+            real_value = values[index] << shift_distance
+            real_values.append(real_value)
+            # print index, " -> ", bit_length, " -> ", hex(values[index]), " -> ", real_value
+    
+        if self.total_bit_length == 32 or self.total_bit_length == 128:
+            return ipaddress.ip_address(sum(real_values))
+        else:
+            return sum(real_values)
+
+    def disjoin(self, value):
+        # value = bin(value)
+        piece_values = []
+        for index, bit_length in enumerate(self.format_):
+            mask = (self.get_max_value(bit_length) << self.get_bit_shift_distance(self.format_, index)) #creates a mask of all 1's for only the relevant bits
+            real_value = value & mask
+            piece_value = real_value >> self.get_bit_shift_distance(self.format_, index)
+            piece_values.append(piece_value)
+            # print index, " -> ", bit_length, " -> ", format(mask, '032b'), format(real_value, '032b'), " -> ", piece_value
+        return piece_values
+
+    def get_bit_shift_distance(self, format_, index):
+        shift_distance = sum([x for x in format_[index+1::]])
+        return shift_distance
+
+    def validate_value(self, value, bit_length):
+        if 0 <= value <= self.get_max_value(bit_length):
+            return True
+        raise Exception("%s out of range (0 -> %s)" % (value, self.get_max_value(bit_length)))
+
+    def validate_list_lengths(self, values):
+        if len(self.format_) != len(values):
+            raise Exception("Format List Length (%s) is not equal to Values List Length (%s)" % (len(self.format_), len(values)))
+        if len(self.format_) == 0:
+            raise Exception("List length can not be 0")
+        return True
+
+    def get_max_value(self, bit_length):
+        return (1 << bit_length) - 1
+
---
+++ tests.py
@@ -0,0 +1,69 @@
+import unittest
+import ipaddress
+from BitDoodle import BitDoodle
+class TestStringMethods(unittest.TestCase):
+
+    def test_joins(self):
+        format_ = [40, 6, 4, 5, 5, 4, 64]
+        values = [0x2607f6f099, 0xb, 0x7, 0xa, 0x9, 15, 0x123]
+        dood = BitDoodle(format_)
+        self.assertEqual(ipaddress.IPv6Address(u'2607:f6f0:992d:d49f::123'), dood.join(values))
+
+        format_ = [8, 4, 6, 4, 6, 2, 2]
+        values = [10, 10, 10, 4, 30, 2, 1]
+        dood = BitDoodle(format_)
+        self.assertEqual(ipaddress.IPv4Address(u'10.162.145.233'), dood.join(values))
+
+        format_ = [8, 8, 4, 4]
+        values = [255, 100, 10, 5]
+        dood = BitDoodle(format_)
+        self.assertEqual(16737445, dood.join(values))
+
+        format_ = [40, 6, 4, 5, 5, 4, 64, 40, 24]
+        values = [0x2607f6f099, 20, 10, 4, 30, 10, 35, 0x74317654, 16000000]
+        dood = BitDoodle(format_)
+        self.assertEqual(932520146808411025667857402430819217206404077264861144064, dood.join(values))
+
+        format_ = [8, 8, 8, 8]
+        values = [200, 200, 500, 200]  #500 too big for 8 bits
+        dood = BitDoodle(format_)
+        with self.assertRaises(Exception):
+            dood.join(values)
+
+        format_ = [8, 8, 8, 8]
+        values = [200, 200, 200, 200, 200]  #too many values
+        dood = BitDoodle(format_)
+        with self.assertRaises(Exception):
+            dood.join(values)
+
+    def test_disjoins(self):
+        format_ = [8, 8, 8, 8]
+        value = 0b00001010000101000001111000101000
+        expected_list = [10, 20, 30, 40]
+        self.assertEqual(expected_list, BitDoodle(format_).disjoin(value))
+
+        format_ = [8, 8, 8, 8]
+        value = 0xffeeddcc
+        expected_list = [255, 238, 221, 204]
+        self.assertEqual(expected_list, BitDoodle(format_).disjoin(value))
+
+        format_ = [4, 4, 8, 8, 8]
+        value = 0xffeeddcc
+        expected_list = [15, 15, 238, 221, 204]
+        self.assertEqual(expected_list, BitDoodle(format_).disjoin(value))
+
+    def test_fullcircle(self):
+        #given a format and values, join the values, then disjoin them, and the values should be the same as the original values
+        format_ = [23, 15, 100, 42, 65]
+        values = [100, 200, 300, 400, 500]
+        dood = BitDoodle(format_)
+        self.assertEqual(values, dood.disjoin(dood.join(values)))
+
+        #given a format and value, disjoin the value, then join the pieces, and the value should be the same as the original value
+        format_ = [23, 15, 100, 42, 65]
+        value = 100200300400500600700800
+        dood = BitDoodle(format_)
+        self.assertEqual(value, dood.join(dood.disjoin(value)))
+
+if __name__ == '__main__':
+    unittest.main()
