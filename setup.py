340dd4ba2d748a7edebf145c96ce9b2f167e91b1
Jared Newell <jnewell@box.com>
2017-09-13 13:26:46 -0700

    module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bitdoodle',
+    version='0.1',
+    packages=['bitdoodle',],
+    license='None',
+    long_description='Doodle the bits',
+    install_requires=[
+        'ipaddress',
+    ]
+)
